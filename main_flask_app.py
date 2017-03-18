c9265caa8ba1b3ccc62748c62338ccee14c06fa5
Bhawna4tech <bhawna21195@gmail.com>
2017-03-19 00:57:31 +0530

    <Bhawna> | Adding csv file generation and showing on webpage

--- main_flask_app.py
+++ main_flask_app.py
@@ -5,6 +5,9 @@ from flask import Flask, render_template, request,send_from_directory, redirect,
 from werkzeug import secure_filename
 from facepp import API, File
 from pprint import pformat
+import time
+import math
+import csv
 
 app = Flask(__name__)
 
@@ -60,19 +63,80 @@ def processing(name):
 	
 	result = api.detection.detect(img = File(os.path.join('uploads/',str(name)))) #to detect   local images
 	#result = api.detection.detect(url=url)  to detect online images
-	return print_result(result)
+	return attributeExtractionToCsv(result)
+	#return print_result(result)
 	
 	#return os.path.join('uploads/',name)
 	#api.group.delete(group_name = 'test')
 	#api.person.delete(person_name = [i[0] for i in PERSONS])
 
+def attributeExtractionToCsv(result):
+	face_id = result['face'][0]['face_id']
+	eye_left_x = result['face'][0]['position']['eye_left']['x']
+	eye_left_y = result['face'][0]['position']['eye_left']['y']
+	eye_right_x = result['face'][0]['position']['eye_right']['x']
+	eye_right_y = result['face'][0]['position']['eye_right']['y']
+	nose_x = result['face'][0]['position']['nose']['x']
+	nose_y = result['face'][0]['position']['nose']['y']
+	mouth_left_x = result['face'][0]['position']['mouth_left']['x']
+	mouth_left_y = result['face'][0]['position']['mouth_left']['y']
+	mouth_right_x = result['face'][0]['position']['mouth_right']['x']
+	mouth_right_y = result['face'][0]['position']['mouth_right']['y']
+	img_height = result['img_height']
+	#result = api.detection.detect(url=url)  to detect online images
 
+	csv_details = [{
+	'Face Id' : face_id,
+	'Distance b/w left eye and nose' : math.sqrt(((eye_left_x-nose_x)*(eye_left_x-nose_x))+((eye_left_y-nose_y)*(eye_left_y-nose_y))),
+	'Distance b/w right eye and nose' : math.sqrt(((eye_right_x-nose_x)*(eye_right_x-nose_x))+((eye_right_y-nose_y)*(eye_right_y-nose_y))),
+	'Distance b/w nose and left side of mouth' : math.sqrt(((nose_x-mouth_left_x)*(nose_x-mouth_left_x))+((nose_y-mouth_left_y)*(nose_y-mouth_left_y))),
+	'Distance b/w nose and right side of mouth' : math.sqrt(((nose_x-mouth_right_x)*(nose_x-mouth_right_x))+((nose_y-mouth_right_y)*(nose_y-mouth_right_y)))
+	}]
+	fields = ['Face Id' , 'Distance b/w left eye and nose' , 'Distance b/w right eye and nose' , 'Distance b/w nose and left side of mouth' , 'Distance b/w nose and right side of mouth']
+	
+	initializeTrainedCSV(fields)
+	writeToCSV("trainedData.csv", fields, csv_details)
+	return uploaded_file(csv_details)
+	
+	#return render_template('csvToTable.html',result = csv_details)
+
+def writeToCSV(fileName, fields, csv_details):
+	with open(fileName,'a') as csvfile:
+		writer = csv.DictWriter(csvfile,fieldnames = fields)
+		writer.writerows(csv_details)
+		
+def initializeTrainedCSV(fields):
+	with open("trainedData.csv",'w') as csvfile:
+		writer = csv.DictWriter(csvfile,fieldnames = fields)
+		writer.writeheader()
+		
+def uploaded_file(filename):
+
+	reader = csv.reader(open("trainedData.csv"))
+	htmlfile = open("templates/csvpage.html","w")
+	#htmlfile=os.fdopen(os.open("templates/hello2.html", os.O_WRONLY | os.O_CREAT, 0600), '-rwx') 
+	rownum = 0
+	htmlfile.write('<table>')
+	for row in reader: 
+		if rownum == 0:
+			htmlfile.write('<tr>')
+		  	for column in row:
+  				htmlfile.write('<th>' + column + '</th>')
+		  	htmlfile.write('</tr>')
+	  	else:
+	  		htmlfile.write('<tr>')	
+  			for column in row:
+  				htmlfile.write('<td>' + column + '</td>')
+	 		htmlfile.write('</tr>')
+		rownum += 1
+	htmlfile.write('</table>')
+	print "Created " + str(rownum) + " row table."
+	htmlfile.close()
+	return render_template('csvpage.html')
+	
 if __name__ == '__main__':
     app.run(
         host="0.0.0.0",
         port=int("80"),
         debug=True
     )
-
-							   
-							   
